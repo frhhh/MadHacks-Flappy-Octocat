8d9453a29361769aa1214daaca2efcc7991a7ebc
Frank Hu <hu353@purdue.edu>
2017-02-19 02:21:00 -0600

    Update Random function

--- FlappyOctocat/RandomFunction.swift
+++ FlappyOctocat/RandomFunction.swift
@@ -8,3 +8,17 @@
 
 import Foundation
 import CoreGraphics
+
+public extension CGFloat{
+    
+    
+    public static func random() -> CGFloat{
+        
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    
+    public static func random(min : CGFloat, max : CGFloat) -> CGFloat{
+        
+        return CGFloat.random() * (max - min) + min
+    }
+}
